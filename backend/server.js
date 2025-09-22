7894da981572608a6a0a74f2bbeab97b6caf64df
afrilelo111 <afrilelo111@gmail.com>
2025-09-23 00:11:45 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-const PORT = 3000;
 
 // Middleware
 app.use(cors());
@@ -50,4 +49,5 @@ app.put("/tasks/:id", async (req, res) => {
 });
 
 // Jalankan server
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
